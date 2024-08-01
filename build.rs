d1d1fb9f6ade3726636a7377d6ab7b9b3f817e67
14ROVI <29734170+14ROVI@users.noreply.github.com>
2024-08-01 02:20:12 +0100

    clean up

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 fn main() {
     Command::new("cmd")
         .arg("/C")
-        .arg("tailwindcss -i assets/tailwind.css -o assets/main.css --minify")
+        .arg("tailwindcss -i tailwind.css -o assets/main.css --minify")
         .output()
         .expect("Failed to compile tailwind css!");
 }
